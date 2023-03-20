18c27b771188a3d0906dcf682e4241d9a38081be
Elías Zelada <elixs.777@gmail.com>
2023-03-20 13:06:34 -0300

    remove comment

--- scenes/player.gd
+++ scenes/player.gd
@@ -8,12 +8,10 @@ const ACCELERATION = 4000
 
 
 func _physics_process(delta):
-	# Add the gravity.
 	
 	if not is_on_floor():
 		velocity.y += GRAVITY * delta
 
-	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	
